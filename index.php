44c98e978b29c5c388eed04d876b2cad2411bed3
Correct Name <BrandonOkeke@icloud.com>
2017-11-30 15:27:32 +0000

    Fetches the latest stuff

--- index.php
+++ index.php
@@ -3,11 +3,18 @@
     <head>
         <title>Brawrdon - Oh Boy</title>
         <?php include 'includes/header.php';?>
+        <?php  ?>
     </head>
     <body>
         <main>
         <?php
+            include 'includes/connect.php';
             include 'includes/navigation.php';
+            $worksSQL = "SELECT work_title, work_url FROM works ORDER BY work_id DESC LIMIT 1";
+            $worksResult = $connection->query($worksSQL);
+
+            $postsSQL = "SELECT post_title, post_slug FROM posts ORDER BY post_id DESC LIMIT 1";
+            $postsResult = $connection->query($postsSQL);
             navigation(true, "nav-item", "nav-item", "nav-item", "nav-item");
         ?>
             <div class="home-jumbotron">
@@ -21,11 +28,32 @@
                         <div class="page-margin">
                             <div class="half-one">
                                 <h2>Latest Work</h2>
-                                <span><a class="subtitle" href="">Pretend This is a Title For a Post</a></span>
+                                <span>
+                                    <?php
+                                        if($worksResult->num_rows > 0) {
+                                            while($row = $worksResult->fetch_assoc()) {
+                                    ?>
+                                    <a class="subtitle" href="<?php echo $row["work_url"];?>"><?php echo $row["work_title"]; ?></a>
+                                    <?php
+                                            }
+                                        }
+                                    ?>
+                                </span>
                             </div>
                             <div class="half-two">
                                 <h2>Latest Opinion</h2>
-                                <span><a class="subtitle" href="">Pretend This is a Title For a Post</a></span>
+                                <span>
+                                    <?php
+                                        if($postsResult->num_rows > 0) {
+                                            while($row = $postsResult->fetch_assoc()) {
+                                    ?>
+                                    <!-- This needs to be changed to work when I have the blog post page set up-->
+                                    <a class="subtitle" href="#<?php echo $row["post_slug"];?>"><?php echo $row["post_title"]; ?></a>
+                                    <?php
+                                            }
+                                        }
+                                    ?>
+                                </span>
                             </div>
                         </div>
                     </div>
