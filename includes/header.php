fbdeebae43e272aa9b8495e166da93ec40744ff2
Correct Name <BrandonOkeke@icloud.com>
2017-12-12 20:33:27 +0000

    Actually using Google Fonts

--- includes/header.php
+++ includes/header.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/style.css" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Barlow|Space+Mono" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Barlow|Space+Mono|Open+Sans" rel="stylesheet">
