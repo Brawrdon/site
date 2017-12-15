45e5cd227f3f4edba66b433b4d2f05e55edc32e9
Correct Name <BrandonOkeke@icloud.com>
2017-12-15 12:08:11 +0000

    Okay I swear it works now

--- blog.php
+++ blog.php
@@ -1,28 +1,24 @@
+<?php
+$page = $_GET['page'];
+include 'includes/connect.php';
+$limit_offset = ($page - 1) * 3;
+$query = $connection->prepare('SELECT * FROM posts ORDER BY post_id DESC LIMIT ?, 4');
+$query->bind_param('i', $limit_offset);
+$query->execute();
+$result = $query->get_result();
+
+if (mysqli_num_rows($result) > 0) {
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <?php
-        $page = $_GET['page'];
-        if (null == $page || !is_numeric($page)) {
-            header("Location: 404.php");
-            die();
-        } else {
-            include 'includes/connect.php';
-            include 'includes/navigation.php';
-            navigation(false);
-
-            $limit_offset = ($page - 1) * 3;
-            $query = $connection->prepare('SELECT * FROM posts ORDER BY post_id DESC LIMIT ?, 4');
-            $query->bind_param('i', $limit_offset);
-            $query->execute();
-            $result = $query->get_result();
-
-            if (mysqli_num_rows($result) > 0) {
-                include 'includes/header.php'; ?>
+        <?php include 'includes/header.php'; ?>
         <title>Blog - Brawrdon</title>
     </head>
     <body>
-
+        <?php
+            include 'includes/navigation.php';
+            navigation(false);?>
         <main>
             <div class="page-margin">
                 <div class="jumbo">
@@ -30,60 +26,49 @@
                         <h1>Blog</h1>
                     </div>
                 </div>
-
                 <aside>
                     <a class="twitter-timeline" data-height="700" data-dnt="true" data-link-color="#3FC4BA" href="https://twitter.com/Brawrdon?ref_src=twsrc%5Etfw"></a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                 </aside>
-        <?php
-                for ($i = 0; $i < mysqli_num_rows($result); $i++) {
-                    if ($i > 2) {
-                        break;
-                    }
-                    $row = mysqli_fetch_assoc($result); ?>
-                <article class="<?php if ($i == 0) {
-                        ?>post-top <?php
-                    } else {
-                        ?>post<?php
-                    } ?>">
-                <div class="post-image-preview" style="background-image: url('<?php echo $row['post_image']; ?>')"></div>
-                <section class="post-inner">
-
-                    <h2><?php echo $row['post_title']; ?></h2>
-
-                    <div class="post-content">
-                        <p><?php echo $row['post_preview']; ?></p>
-                    </div>
-                </section>
-                <a href="<?php echo '/post.php?post='.$row['post_slug']; ?>"><span class="read-more">Read more</span></a>
-            </article>
-
-        <?php
-                } ?>
-                <div class="post-nav">
-            <?php
-                    if ($page != 1) {
-                        ?>
-                    <a href="blog.php?page=<?php echo $page - 1; ?>"><div class="previous-page"><span>Previous page</span></div></a>
-            <?php
-                    }
-
-            if (mysqli_num_rows($result) > 3) {
-                ?>
-                    <a href="blog.php?page=<?php echo $page + 1; ?>"><div class="next-page"><span>Next page</span></div></a>
-            <?php
-            } ?>
-                </div>
                 <?php
-        } else {
-            header("Location: 404.php");
-            die();
-        }
+                    for ($i = 0; $i < mysqli_num_rows($result); $i++) {
+                        if ($i > 2) {
+                            break;
+                        }
+                        $row = mysqli_fetch_assoc($result);
+                ?>
+                <article class="<?php if ($i == 0) { ?>post-top <?php } else { ?>post<?php } ?>">
+                    <div class="post-image-preview" style="background-image: url('<?php echo $row['post_image']; ?>')"></div>
+                    <section class="post-inner">
+                        <h2><?php echo $row['post_title']; ?></h2>
+                        <div class="post-content">
+                            <p><?php echo $row['post_preview']; ?></p>
+                        </div>
+                    </section>
+                    <a href="<?php echo '/post.php?post='.$row['post_slug']; ?>"><span class="read-more">Read more</span></a>
+                </article>
 
-        $connection = null;
-    }
-        ?>
+            <?php } ?>
+                <div class="post-nav">
+                    <?php
+                        if ($page != 1) {
+                    ?>
+                    <a href="blog.php?page=<?php echo $page - 1; ?>"><div class="previous-page"><span>Previous page</span></div></a>
+                    <?php
+                        }
 
-        </div>
+                        if (mysqli_num_rows($result) > 3) {
+                    ?>
+                    <a href="blog.php?page=<?php echo $page + 1; ?>"><div class="next-page"><span>Next page</span></div></a>
+                <?php } ?>
+                </div>
+            </div>
         </main>
     </body>
 </html>
+<?php
+    $connection = null;
+} else {
+    header("Location: 404.php");
+    die();
+}
+?>
--- post.php
+++ post.php
@@ -1,27 +1,25 @@
+<?php
+    $post = $_GET['post'];
+    include 'includes/connect.php';
+    $query = $connection->prepare('SELECT post_title, post_image, post_content, post_tags, post_date FROM posts WHERE post_slug = ?');
+    $query->bind_param('s', $post);
+    $query->execute();
+    $result = $query->get_result();
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <?php
-        $post = $_GET['post'];
-            include 'includes/connect.php';
-            $query = $connection->prepare('SELECT post_title, post_image, post_content, post_tags, post_date FROM posts WHERE post_slug = ?');
-            $query->bind_param('s', $post);
-            $query->execute();
-            $result = $query->get_result();
-
-            if (mysqli_num_rows($result) > 0) {
-                include 'includes/header.php';
-                $row = mysqli_fetch_assoc($result); ?>
+        <?php include 'includes/header.php'; ?>
         <title><?php echo $row['post_title']; ?> - Brawrdon</title>
     </head>
     <body>
         <?php
-
             include 'includes/navigation.php';
-                include 'includes/Parsedown.php';
-                navigation(false); ?>
-
-
+            include 'includes/Parsedown.php';
+            navigation(false); ?>
         <main>
             <div class="page-margin">
                 <div class="jumbo">
@@ -29,35 +27,29 @@
                         <h1>Blog</h1>
                     </div>
                 </div>
-
-            <article class="post-top">
-                <div class="post-image" style="background-image: url('<?php echo $row['post_image']; ?>')"></div>
-                <section class="post-inner">
-
-                    <h2><?php echo $row['post_title']; ?></h2>
-
-                    <div class="post-content">
+                <aside>
+                    <a class="twitter-timeline" data-height="700" data-dnt="true" data-link-color="#3FC4BA" href="https://twitter.com/Brawrdon?ref_src=twsrc%5Etfw"></a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                </aside>
+                <article class="post-top">
+                    <div class="post-image" style="background-image: url('<?php echo $row['post_image']; ?>')"></div>
+                    <section class="post-inner">
+                        <h2><?php echo $row['post_title']; ?></h2>
+                        <div class="post-content">
                         <?php
                             $Parsedown = new Parsedown();
-                echo $Parsedown->text($row['post_content']); ?>
-                    </div>
-                </section>
-
-            </article>
-
-        <?php
-        $connection = null;
-            } else {
-        header("Location: 404.php");
-        die();
-    }
-
-
-        ?>
-            <aside>
-                <a class="twitter-timeline" data-height="700" data-dnt="true" data-link-color="#3FC4BA" href="https://twitter.com/Brawrdon?ref_src=twsrc%5Etfw"></a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-            </aside>
-        </div>
+                            echo $Parsedown->text($row['post_content']);
+                        ?>
+                        </div>
+                    </section>
+                </article>
+        <?php $connection = null; ?>
+            </div>
         </main>
     </body>
 </html>
+<?php
+    } else {
+        header("Location: 404.php");
+        die();
+    }
+?>
