9a39450218a0a7d5d1e01c5a0d5416bd79a228ee
Correct Name <BrandonOkeke@icloud.com>
2018-01-19 12:07:34 +0000

    Fixed Works on the homepage

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
             include 'includes/connect.php';
             include 'includes/navigation.php';
             navigation(true);
-            $worksSQL = "SELECT work_title, work_url FROM works ORDER BY work_id DESC LIMIT 1";
+            $worksSQL = 'SELECT post_title, post_slug FROM posts WHERE post_type = "w" ORDER BY post_id DESC LIMIT 1';
             $worksResult = $connection->query($worksSQL);
 
-            $postsSQL = "SELECT post_title, post_slug FROM posts ORDER BY post_id DESC LIMIT 1";
+            $postsSQL = 'SELECT post_title, post_slug FROM posts WHERE post_type = "b" ORDER BY post_id DESC LIMIT 1';
             $postsResult = $connection->query($postsSQL);
         ?>
             <div class="home-jumbotron">
@@ -28,29 +28,30 @@
                     <section class="second">
                         <div class="page-margin">
                             <div class="extra-padding">
-                            <section class="half-one">
-                                <div class="subtitle">Latest Work</div>
-                                <div>
-                                    <?php
-                                        if($worksResult->num_rows > 0) {
-                                            while($row = $worksResult->fetch_assoc()) {
-                                    ?>
-                                    <a class="post-title" href="<?php echo $row["work_url"];?>"><?php echo $row["work_title"]; ?></a>
-                                    <?php
+                                <section class="half-one">
+                                    <div class="subtitle">Latest Work</div>
+                                    <div>
+                                        <?php
+                                            if ($worksResult->num_rows > 0) {
+                                                while ($row = $worksResult->fetch_assoc()) {
+                                                    ?>
+                                        <a class="post-title" href="post.php?post=<?php echo $row["post_slug"]; ?>"><?php echo $row["post_title"]; ?></a>
+                                        <?php
+                                                }
                                             }
-                                        }
-                                    ?>
-                                </div>
-                            </section>
+
+                                            $connection = null;
+                                        ?>
+                                    </div>
+                                </section>
                             <section class="half-two">
                                 <div class="subtitle">Latest Opinion</div>
                                 <div>
                                     <?php
-                                        if($postsResult->num_rows > 0) {
-                                            while($row = $postsResult->fetch_assoc()) {
-                                    ?>
-                                    <!-- This needs to be changed to work when I have the blog post page set up-->
-                                    <a class="post-title" href="post.php?post=<?php echo $row["post_slug"];?>"><?php echo $row["post_title"]; ?></a>
+                                        if ($postsResult->num_rows > 0) {
+                                            while ($row = $postsResult->fetch_assoc()) {
+                                                ?>
+                                    <a class="post-title" href="post.php?post=<?php echo $row["post_slug"]; ?>"><?php echo $row["post_title"]; ?></a>
                                     <?php
                                             }
                                         }
