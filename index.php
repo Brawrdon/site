036f5d9504992ab2eb14112b27f017c2a064fa40
Correct Name <BrandonOkeke@icloud.com>
2019-01-03 18:30:28 +0000

    Add navigation

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Brandon Okeke - Oh Boy</title>
+    <meta name="description"
+          content="I'm Brandon Okeke. A Computer Science undergraduate and a creative who hasn't created anything">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <header>
+        <div class="container">
+            <nav>
+                <ul>
+                    <li><a href="" class="last-nav-item">the big lists</a></li>
+                    <li><a href="">work</a></li>
+                    <li><a href="">blog</a></li>
+                    <li><a href="">about</a></li>
+                    <li class="brawrdon"><a href="/">brawrdon</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+</body>
+</html>
